906aab7e5cd3174c3996526548b9c7724dbcd6f0
fcracker79 <fcracker79@gmail.com>
2017-04-03 22:39:37 +0200

    Added basic support for JSP.

---
+++ src/main/java/io/mirko/controllers/mvc/MainController.java
@@ -0,0 +1,17 @@
+package io.mirko.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "This is a test message");
+        return "index";
+    }
+}
---
+++ src/test/java/io/mirko/controllers/mvc/MainControllerTest.java
@@ -0,0 +1,30 @@
+package io.mirko.controllers.mvc;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class MainControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testJspWithEl() throws Exception {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+        assertThat(entity.getStatusCode(), IsEqual.equalTo(HttpStatus.OK));
+        assertThat(entity.getBody(), containsString("/resources/text.txt"));
+    }
+}
