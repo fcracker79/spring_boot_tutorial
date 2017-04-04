f79b3f63efa332a7e7ed8d249868662254a00760
fcracker79 <fcracker79@gmail.com>
2017-04-05 00:45:06 +0200

    Give up using 2.0.0-SNAPSHOT: things do not work as expected.

--- src/main/java/io/mirko/controllers/mvc/MainController.java
+++ src/main/java/io/mirko/controllers/mvc/MainController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class MainController {
     @GetMapping("/")
     public String mainPage(Map<String, Object> model) {
+        System.out.println("Dino************************************");
         model.put("time", new Date());
         model.put("message", "This is a test message");
         return "index";
